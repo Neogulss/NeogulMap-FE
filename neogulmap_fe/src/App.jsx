1371e1ba85ecdc2679e730fd95fa575644372351
godong <godong@godongui-MacBookPro.local>
2026-03-30 16:34:13 +0900

    chore:tailwind setting

--- neogulmap_fe/src/App.jsx
+++ neogulmap_fe/src/App.jsx
@@ -9,6 +9,15 @@ function App() {
 
   return (
     <>
+    <div className="p-10">
+      <h1 className="text-h-1">Heading 1</h1>
+      <h2 className="text-h-2">Heading 2</h2>
+      <h3 className="text-h-3">Heading 3</h3>
+
+      <p className="text-b-1 mt-4">Body Large</p>
+      <p className="text-b-2">Body Medium</p>
+      <p className="text-b-3">Body Small</p>
+    </div>
       <section id="center">
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
