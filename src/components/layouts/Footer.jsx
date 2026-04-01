42eb7db3daa5971c6f1950b74bfd5c30f2ea9a82
goodoong <yunsun4830@gmail.com>
2026-04-01 10:05:10 +0900

    feat: 헤더, 푸터, custombutton 변경

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -2,17 +2,21 @@ import React from "react";
 import { Link } from "react-router-dom";
 export default function Footer() {
   return (
-    <footer className="bg-[#1a1a1a] text-[#a3a3a3] px-10 py-12 text-xs">
-      <div className="max-w-7xl mx-auto">
-        <div className="flex justify-between items-start mb-10">
-          <div className="flex items-center gap-2 text-white text-lg font-bold">
-            <span className="text-xl">🦝</span>
+    <footer className="bg-[#121415] text-neutral-5 px-10 py-8 text-xs">
+      <div className="mx-auto max-w-7xl">
+        <div className="flex items-start justify-between mb-10">
+          <div className="flex items-center gap-2 text-lg font-bold text-neutral-2">
+            <img
+            src="src/assets/images/logo.svg"
+            alt="입지너구리 로고"
+            className="object-contain w-auto h-8 bg-white rounded-full "
+          />
             <span>입지너구리</span>
           </div>
 
           <div className="text-right">
-            <p className="text-white mb-1">고객센터</p>
-            <p className="text-white text-2xl font-bold mb-2">1588-0000</p>
+            <p className="mb-1 text-white">고객센터</p>
+            <p className="mb-2 text-2xl font-bold text-white">1588-0000</p>
             <p className="text-white">
               평일 09:00 - 18:00 (점심시간 12:00 - 13:00)
             </p>
@@ -20,8 +24,8 @@ export default function Footer() {
           </div>
         </div>
 
-        <div className="border-t border-[#333] pt-8 flex justify-between items-center">
-          <div className="text-[#666]">
+        <div className="flex items-center justify-between pt-8 border-t border-neutral-9">
+          <div className="text-neutral-5">
             © 2026 IPJI-NEOGURI Inc. All rights reserved.
           </div>
         </div>
