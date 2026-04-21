d59395d3a45718467e79a023109bba72dd745252
goodoong <yunsun4830@gmail.com>
2026-04-21 12:05:46 +0900

    design: 페이지 이동시 자동 페이지 상단 이동 적용

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import './App.css'
 import { BrowserRouter } from "react-router-dom";
 import Router from './routes/Router.jsx';
 import AlertContainer from './components/common/AlertContainer.jsx';
+import ScrollToTop from './components/layouts/ScrollToTop.jsx';
 function App() {
 
    return (
     <BrowserRouter>
+    <ScrollToTop />
       <Router />
       <AlertContainer />
     </BrowserRouter>
